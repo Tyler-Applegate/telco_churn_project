631ea2962253e79574a213ae29f513575a3a41da
Tyler-Applegate <tyler.applegate@gmail.com>
2021-05-25 11:36:57 -0500

    checking formatting

--- acquire.py
+++ acquire.py
@@ -6,8 +6,16 @@ import numpy as np
 import os
 
 ############### Connection #####################################################
-# Enables access to my env.py file in order to use sensitive infor to access Codeup DB
+
+# Enables access to my env.py file in order to use sensitive info to access Codeup DB
 from env import host, user, password
+
+# sets up a secure connection to the Codeup db using my login infor
+def get_db_url(db, user=user, host=host, password=password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+# assigns the telco_churn url to the variable name 'url' so it can be used in additional functions
+url = get_db_url('telco_churn')
 
 def get_connection(db, user=user, host=host, password=password):
     '''
@@ -21,11 +29,11 @@ def new_telco_data():
     This function joins the 'customers', 'contract_types', 'internet_service_types', and 'payment_types' tables from the telco_churn db
     and return a pandas DataFrame with all columns/values from all tables.
     '''
-    sql_query = 'SELECT * 
-                FROM customers
-                JOIN contract_types USING(contract_type_id)
-                JOIN internet_service_types USING(internet_service_type_id)
-                JOIN payment_types USING(payment_type_id)'
+    sql_query = '''SELECT * 
+                    FROM customers
+                    JOIN contract_types USING(contract_type_id)
+                    JOIN internet_service_types USING(internet_service_type_id)
+                    JOIN payment_types USING(payment_type_id)'''
     return pd.read_sql(sql_query, get_connection('telco_churn'))
 
 def get_telco_data():
